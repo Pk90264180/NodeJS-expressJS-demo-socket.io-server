9599e46bdecab0934f40c0edc375bc6a93e04948
pk90264180 <pk90264180@gmail.com>
2023-07-20 12:32:20 +0530

    after recreating munchai

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { useState } from 'react';
 
 import * as io from 'socket.io-client/dist/socket.io.js';
-const socket = io.connect('https://backend-exeprimental.onrender.com/');
+// const socket = io.connect('http://localhost:3001');
+const socket = io.connect('https://mun-chai-backend.onrender.com');
 
 function App() {
   const [id, setId] = useState('');
   const [status, setStatus] = useState('');
   socket.on('send_message', (arg) => {
-    console.log(arg.id); // world
-    setId(arg.id);
+    console.log(arg.order_id); // world
+    setId(arg.order_id);
     setStatus(arg.status);
   });
   return (
